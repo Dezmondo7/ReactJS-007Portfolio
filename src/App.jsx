11bfa1b79a1e61e33c5983898e43594299dcaf9c
Dale Warburton <dalewarby23@outlook.com>
2024-01-30 18:33:06 +0000

    fixed bootstrap components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import './App.css'
 import React from "react";
 import Header from './components/Header';
 import TopImage from './components/TopImage';
+import Bootstrap from './components/Bootstrap';
+
 
 function App() {
+
   return <div>
    <Header />
    <TopImage />
+  <Bootstrap />
    </div>
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,24 +3,23 @@ import React from "react";
 function Header() {
     return    <header>
         <div class="navbar">
-            <div class="logo"><a href="index.html">Dezmondo7 Portfolio</a></div>
-            <ul class="links">
-                <li><a href="#box-a">About Me</a></li>
+            <div className="logo"><a href="index.html">Dezmondo7 Portfolio</a></div>
+            <ul className="links">
+                <li><a href="#box-a">Home</a></li>
                 <li><a href="#box-c">Projects</a></li>
-                <li><a href="netflix.html">Netflix</a></li>
                 <li><a href="#contactme">Contact Me</a></li>
             </ul>
-            <a href="#" class="action_button">Cirriculum Vitae</a>
-            <div class="toggle_button">
-                <i class="fa-solid fa-bars"></i>
+            <a href="#" className="action_button">Cirriculum Vitae</a>
+            <div className="toggle_button">
+                <i className="fa-solid fa-bars"></i>
             </div>
         </div>
-        <div class="dropdown_menu">
+        <div className="dropdown_menu">
             <li><a href="hero">About Me</a></li>
             <li><a href="about">Projects</a></li>
             <li><a href="services">Contact Me</a></li>
             <li><a href="contact">Cirriculem Vitae</a></li>
-            <li><a href="#" class="action_button"></a></li>
+            <li><a href="#" className="action_button"></a></li>
         </div>
     </header>
 }

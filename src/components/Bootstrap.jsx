d0a0f371c34bf9344247eb290cb7c11fd619d237
Dale Warburton <dalewarby23@outlook.com>
2024-01-31 18:29:51 +0000

    added style to the card and folder for CSS components

--- src/components/Bootstrap.jsx
+++ src/components/Bootstrap.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import myPhoto from "../images/mypic.jpg";
 
 function Bootstrap() {
     return  <div className="card" >
-    <img src="..." className="card-img-top" alt="..." />
+    <img src={myPhoto} className="card-img-top" alt="Image of me." />
     <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" className="btn btn-primary">Go somewhere</a>
+      <h5 className="card-title">Dale Warburton</h5>
+      <p className="card-text">HTML5, CSS3, JavaScript, Python, JQuery, React, Bootstrap 5.3, Git
+        .</p>
+      <a href="#" className="btn btn-primary">Projects</a>
     </div>
   </div>
 }

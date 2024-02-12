4875d58acfa878b86f71b5cca3b7898200d74ded
Dale Warburton <dalewarby23@outlook.com>
2024-02-12 22:50:48 +0000

    Added a navbar and footer along with a carousel component for the projects page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import React from "react";
 import Home from './pages/Home';
+import NavBar from './pages/NavBar';
+import Footer from './Footer';
 
 
 function App() {
@@ -10,11 +12,13 @@ function App() {
   <div className='page-container'>
     <div className='contenet-wrapper'>
      <Router>
+      <NavBar />
      <Routes>
      <Route path="/" element={<Home />} />
      </Routes >
      </Router >
    </div>
+     <Footer />
    </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import myImage from "../images/header-image.avif";
 import myPhoto from "../images/mypic.jpg";
-
+import './styles.css'
 
 function Home() {
 
     return  ( <div className="top-image">
     <img src={myImage} height="200px" width="100%" alt="faded orange strip going across the page with the words project work in white in the bottom right corner" />
-    <div className="bottom-right">
-        <p>Project Work</p>
-    </div>
     <div className="card" >
     <img src={myPhoto} className="card-img-top" alt="Image of me." />
     <div className="card-body">
@@ -20,6 +17,8 @@ function Home() {
     </div>
   </div>
   </div>
+
+
     );
 }
 

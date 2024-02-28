a20a2a09652b68cb558cc05b5821079588eb75da
Dale Warburton <dalewarby23@outlook.com>
2024-02-28 00:36:49 +0000

    updated cards to bootsrap and added a button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 function Footer() {
-    return ( <footer className="bg-body-tertiary text-center text-lg-start">
+    return ( 
+    <div class="wrapper">
+    <footer className="bg-body-tertiary text-center text-lg-start">
     <div className="text-center p-3">
       © 2024 Copyright:
       <a className="text-body" href="https://mdbootstrap.com/">DezmondoReactJS</a>
     </div>
-  </footer> )
+  </footer>
+  </div>
+   )
 }
 export default Footer;
--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -1,26 +1,48 @@
 import React from "react";
-import { FaShoppingCart, FaRegBookmark, FaFireAlt, } from 'react-icons/fa';
+import '../components/Card';
 
 function Cards(props) {
-    return(
-        <div className='productList'>
-            <div key={props.id} className='productCard'></div>
-            <img src={props.image} alt="" className="productImage"></img>
-
-            <FaShoppingCart className={"productCard__cart"} />
-            <FaRegBookmark className={"productCard__wishlist"} />
-            <FaFireAlt className={"productCard__fastSelling"} />
-
-            <div className='productcard__content'>
-                <h3 className='productName'>{props.name}</h3>
-                <div className='displaystack__1'></div>
-                <div className='productProce'>${props.price}</div>
-                <div className='productSales'>{props.totalSales} units sold</div>
-            </div>    
-            <div className='displayStack__2'>
-                <div className='productTime'>{props.timeLeft} days left</div>
-            </div>
-            </div>
+    return( 
+        <body>
+        <div class="pageContainer">
+        <div class="card-group">
+  <div class="card">
+    <img src="..." class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <button type="button" class="btn btn-outline-primary">Primary</button>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <button type="button" class="btn btn-outline-primary">Primary</button>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <button type="button" class="btn btn-outline-primary">Primary</button>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+</div>
+</div>
+</body>
+       
     )
 }
 export default Cards; 

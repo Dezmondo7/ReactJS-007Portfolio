a63126979502a3dedba24ac5899a399038df8aad
Dale Warburton <dalewarby23@outlook.com>
2024-03-07 19:36:04 +0000

    updated logo position on navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,31 @@ function Home() {
     return  ( <div className="top-image">
     <img src={myImage} height="200px" width="100%" alt="faded orange strip going across the page with the words project work in white in the bottom right corner" />
   {/*New Hero Section*/}
+  <section class="mb-40">
+
+  <div class="px-6 py-12 text-center md:px-12 lg:text-left">
+    <div class="w-100 mx-auto sm:max-w-2xl md:max-w-3xl lg:max-w-5xl xl:max-w-7xl xl:px-32">
+      <div class="grid items-center lg:grid-cols-2">
+        <div class="mb-12 md:mt-12 lg:mt-0 lg:mb-0">
+          <div
+            class="block rounded-lg bg-[hsla(0,0%,100%,0.55)] px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-[hsla(0,0%,5%,0.55)] dark:shadow-black/20 md:px-12 lg:-mr-14 backdrop-blur-[30px]">
+            <h1 class="mt-2 mb-16 text-4xl font-bold tracking-tight md:text-5xl xl:text-6xl">
+              Welcome to my  <br /><span class="text-primary">ReactJS Portfolio</span>
+            </h1>
+            <a class="mb-2 inline-block rounded bg-primary px-12 pt-4 pb-3.5 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] md:mr-2 md:mb-0"
+              data-te-ripple-init data-te-ripple-color="light" href="#!" role="button">Cirriculem Vitae</a>
+          </div>
+        </div>
+        <div class="md:mb-12 lg:mb-0">
+          <img src="https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?q=80&w=1364&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            class="w-full rounded-lg shadow-lg dark:shadow-black/20" alt="Laptop on a desk with earphones and mobile phone" />
+        </div>
+      </div>
+    </div>
+  </div>
+
+</section>
+
   {/*About me section*/}
   <div class="container my-24 mx-auto md:px-6">
 
@@ -65,7 +90,7 @@ function Home() {
               <li>Level 2 Website Software (ITQ)</li>
               <li>Level 2 Using Collaborative Technologies (ITQ)</li>
               <li>Level 2 IT User Skills (Open Systems and Enterprise) (ITQ)</li>
-              <li>EdX Front-end Engineer Bootcamp Alumni</li>
+              <li>2024 EdX Front-end Engineer Bootcamp Alumni</li>
               </ul>
             </p>
           </div>
--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -37,7 +37,7 @@ export default function Example() {
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                  <img className="h-8 w-auto"
+                  <img className="h-8"
                     src={reactLogo}
                     alt="Your Company"
                   />

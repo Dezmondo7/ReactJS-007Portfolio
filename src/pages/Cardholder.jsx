42540131378b8a711c405a72cb310635efa65ed6
Dale Warburton <dalewarby23@outlook.com>
2024-04-08 13:49:21 +0100

    updated image filepaths

--- src/pages/Cardholder.jsx
+++ src/pages/Cardholder.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import '../pages/Cardholder.css';
-import hks from '../images/hks.jpg'
 
 function Cardholder(props) {
     return (

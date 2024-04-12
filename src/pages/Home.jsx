d2c90c851639af9bcea80bb2f597388d4d3ab0cb
Dale Warburton <dalewarby23@outlook.com>
2024-04-12 17:53:54 +0100

    added testimonial

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -366,13 +366,12 @@ function Home() {
               xmlns="http://www.w3.org/2000/svg"
               fill="currentColor"
               viewBox="0 0 448 512">
-
+              
                 <path
                   d="M0 216C0 149.7 53.7 96 120 96h8c17.7 0 32 14.3 32 32s-14.3 32-32 32h-8c-30.9 0-56 25.1-56 56v8h64c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V320 288 216zm256 0c0-66.3 53.7-120 120-120h8c17.7 0 32 14.3 32 32s-14.3 32-32 32h-8c-30.9 0-56 25.1-56 56v8h64c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H320c-35.3 0-64-28.7-64-64V320 288 216z" />
               </svg>
             </span>
-            Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis
-            suscipit laboriosam, nisi ut aliquid commodi.
+            "In my time working with Dale, he was one of the top performing consultants on the team."
           </p>
           <ul className="mb-0 flex items-center justify-center">
             <li>
@@ -458,7 +457,7 @@ function Home() {
                   d="M0 216C0 149.7 53.7 96 120 96h8c17.7 0 32 14.3 32 32s-14.3 32-32 32h-8c-30.9 0-56 25.1-56 56v8h64c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V320 288 216zm256 0c0-66.3 53.7-120 120-120h8c17.7 0 32 14.3 32 32s-14.3 32-32 32h-8c-30.9 0-56 25.1-56 56v8h64c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H320c-35.3 0-64-28.7-64-64V320 288 216z" />
               </svg>
             </span>
-            Dale is a great asset to RWL. Hes a very diligent, focused, driven individual who delivers and manages his duties above and beyond.
+            "Dale is a great asset to RWL. Hes a very diligent, focused, driven individual who delivers and manages his duties above and beyond."
           </p>
           <ul className="mb-0 flex items-center justify-center">
             <li>

15cc99123acdc5480ba40d417079630af9684457
Dale Warburton <dalewarby23@outlook.com>
2025-02-23 13:02:31 +0000

    added navbar start to branch

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,148 +1,63 @@
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import navPic from '../images/navPic.jpg';
-import reactLogo from '../images/reactLogo.png';
+import { useState, useEffect } from "react";
+import { Navbar, Container } from "react-bootstrap";
+import logo from '../assets/spyroweb.png';
+import { NavLink } from "react-router-dom";
 import '../pages/NavBar.css';
+{/*import navIcon1 from '';
+import navIcon2 from '';
+import navIcon3 from '';*/}
 
-const navigation = [
-  { name: 'Home', href: '#', current: true },
-  { name: 'Projects', href: '#/Projects', current: false },
-  { name: 'Github', href: '#/Github', current: false },
-  { name: 'Contact', href: '#/Contact', current: false },
-]
+import React from 'react';
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
+function NavBar() {
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, seScrolled] = useState(false);
 
-export default function Navbar() {
+  useEffect(() => {
+     const onScroll = () => {
+      if (window.scrollY > 50) {
+        seScrolled(fasle);
+      }
+     }
+
+     window.addEventListener("scroll", onScroll);
+
+     return () => window.removeEventListener("scroll", onScroll)
+  }, [])
+
+  const onUpdateActieLink = (value) => {
+    setActiveLink(value);
+  }
+
+
   return (
-    <Disclosure as="nav" className="bg-gray-800">
-      {({ open }) => (
-        <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="absolute -inset-0.5" />
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
+    <Navbar  expand="lg" className={scrolled ? "scrolled": ""}>
+      <Container>
+        <Navbar.Brand href="#Home">
+          <img src={logo} alt="Logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle ari-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon">/</span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Navbar className="me-auto">
+              <NavLink href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActieLink('home')}>Home</NavLink>
+              <NavLink href="#Skils" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActieLink('skills')}>Skills</NavLink>
+              <NavLink href="#Projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActieLink('projects')}>Projects</NavLink>
+            </Navbar>
+            <span className="Navbar-text">
+              <div className="social-icon">
+                <a href="#"><img src={""} alt="" /></a>
+                <a href="#"><img src={""} alt="" /></a>
+                <a href="#"><img src={""} alt="" /></a>
               </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
-                  <img className="h-8"
-                    src={reactLogo}
-                    alt="Your Company"
-                  />
-                </div>
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
-                </div>
-              </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
+            </span>
+        </Navbar.Collapse>
 
-                {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-3">
-                  <div>
-                    <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="absolute -inset-1.5" />
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src={navPic}
-                        alt="Picture of Dale Warburton"
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="https://www.linkedin.com/in/dalewarburton1/" target="_blank"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                           <i class="fa-brands fa-linkedin"> LinkedIN</i>
-                           
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="https://github.com/Dezmondo7" target="_blank"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            <i class="fa-brands fa-github"> GitHub</i>
-                          </a>
-                        )}
-                      </Menu.Item>
-                     {/* <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Sign out
-                          </a>
-                        )}
-                        </Menu.Item>*/}
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-              </div>
-            </div>
-          </div>
-
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block rounded-md px-3 py-2 text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
+      </Container>
+    </Navbar>
   )
 }
+
+export default NavBar;

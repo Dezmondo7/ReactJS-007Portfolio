2a6604e5848d778d30ca40c13e842d5f2b08f6a6
Dale Warburton <dalewarby23@outlook.com>
2025-02-24 22:29:57 +0000

    updated App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import Home from './pages/Home';
 import NavBar from './pages/NavBar';
